c63ea63893a5810730b985d3c23613a6ee4a96c2
venh <conf312@naver.com>
2022-07-13 17:51:44 +0900

    #1 initialize spring gateway, [add] custom filter class

---
+++ src/main/java/com/issuemoa/user/gateway/IssuemoaUserGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.issuemoa.user.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuemoaUserGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuemoaUserGatewayApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/issuemoa/user/gateway/filter/ClientAuthFilter.java
@@ -0,0 +1,53 @@
+package com.issuemoa.user.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import java.util.List;
+import java.util.Objects;
+
+@Slf4j
+@Component
+public class ClientAuthFilter extends AbstractGatewayFilterFactory {
+    @Override
+    public GatewayFilter apply(Object config) {
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("ClientAuthFilter : request uri => {}", request.getURI());
+
+            // Request Header 에 X-CLIENT-KEY가 존재하지 않을 때
+            if (!request.getHeaders().containsKey("X-CLIENT-KEY")) {
+                return handleUnAuthorized(exchange); // 401 Error
+            }
+
+            // Request Header 에서 X-CLIENT-KEY 문자열 받아오기
+            List<String> clientKey = request.getHeaders().get("X-CLIENT-KEY");
+            String clientKeyString = Objects.requireNonNull(clientKey).get(0);
+
+            // clientKey 검증
+            if (!clientKeyString.equals("SamQHPleQjbSKeyRvJWElcHJvamVjdCFA")) {
+                return handleUnAuthorized(exchange); // 토큰이 일치하지 않을 때
+            }
+
+            // Custom Post Filter Handler
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                log.info("ClientAuthFilter : response code -> {}", response.getStatusCode());
+            }));
+        });
+    }
+
+    private Mono<Void> handleUnAuthorized(ServerWebExchange exchange) {
+        log.info("ClientAuthFilter : response code -> {}", "401 Unauthorized");
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+        return response.setComplete();
+    }
+}
