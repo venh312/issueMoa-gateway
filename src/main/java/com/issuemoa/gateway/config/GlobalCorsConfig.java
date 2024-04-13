cd3631955fc06b537279ba74edb1babad7ccae32
venh <conf312@naver.com>
2024-04-13 20:59:24 +0900

    [+] [GatewayConfig] CORS 설정 추가

--- src/main/java/com/issuemoa/gateway/config/GlobalCorsConfig.java
+++ src/main/java/com/issuemoa/gateway/config/GlobalCorsConfig.java
@@ -5,16 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import java.util.Arrays;
 
 @Configuration
-public class GatewayConfig {
-
+public class GlobalCorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://issuemoa.kr"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+        configuration.addAllowedOrigin("https://issuemoa.kr"); // HTTPS 도메인 추가
+        configuration.addAllowedMethod("GET");
+        configuration.addAllowedMethod("POST");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
